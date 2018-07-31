c9621089b61f2821a231a0ca55275877e916d4a6
Shujian Wen <jefffreywen94@gmail.com>
2018-07-30 18:07:17 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# FDU Project: BadminTime
+
+- BadminTime: The right place to enjoy badminton at your free time! 
+- This application is deployed on Heroku: link [here](https://fathomless-fortress-10633.herokuapp.com/).
+- To Yuan:
+   - create separate branches for individual work
+   - when there is a need to merge to master branch, make pull request and assign to each other
+   - You can run `ng serve` at `/app_public` to see current progress, port is 
+   `http://localhost:4200`.
+   - To see the progress on main directory, you need to first run `ng build -prod -op build
+` at `/app_public`, then `nodemon` or `npm start` at main directory
+   - 
---
+++ app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+require('./app_api/models/db');
+const apiRoutes = require('./app_api/routes/index');
+
+const app = express();
+
+// view engine setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+
+app.use('/api', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use('/api', apiRoutes);
+
+// send all requests to index.html. Let Angular handle routing.
+// regex determines what kind of urls we'll handle
+app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
