810ed3d08fa1985fa81ad3ee9ca084890018df5a
Shujian Wen <jefffreywen94@gmail.com>
2018-08-01 11:23:20 -0700

    set up routes

---
+++ app_api/controllers/events.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Eve = mongoose.model('Event');
+const Usr = mongoose.model('User');
+
+const getEventList = function (req, res) {
+
+};
+
+const addEvent = function (req, res) {
+
+};
+
+const getEvent = function (req, res) {
+
+};
+
+const modifyEvent = function (req, res) {
+
+};
+
+const deleteEvent = function (req, res) {
+
+};
+
+module.exports = {
+  getEventList,
+  addEvent,
+  getEvent,
+  modifyEvent,
+  deleteEvent
+};
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ctrlEvents = require('../controllers/events');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// events
+router
+    .route('/events')
+    .get(ctrlEvents.getEventList)
+    .post(ctrlEvents.addEvent);
+
+router
+    .route('/events/:eventid')
+    .get(ctrlEvents.getEvent)
+    .put(ctrlEvents.modifyEvent)
+    .delete(ctrlEvents.deleteEvent);
 
 module.exports = router;
