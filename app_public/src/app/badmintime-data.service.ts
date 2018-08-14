dc936b91702c0831c7abffb03f4970132dafda49
gao7yuan <gao7yuan@gmail.com>
2018-08-13 22:29:15 -0700

    deploy

--- app_public/src/app/badmintime-data.service.ts
+++ app_public/src/app/badmintime-data.service.ts
@@ -11,7 +11,7 @@ export class BadmintimeDataService {
   constructor(private http: HttpClient, private auth: AuthenticationService) { }
 
   //private apiBaseUrl = 'http://localhost:3000/api';
-  private apiBaseUrl = 'https:loc8r-yuangao.herokuapp.com/api';
+  private apiBaseUrl = 'https://fathomless-fortress-10633.herokuapp.com';
 
   public getEventList(): Promise<Event[]> {
     const url: string = `${this.apiBaseUrl}/events`;
